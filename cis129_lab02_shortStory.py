90a6c2bde73c509c3cffc2ccc6ab4bbe8a65dfa0
nghiadoan6492 <159955769+nghiadoan6492@users.noreply.github.com>
2024-02-13 18:15:47 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,4 @@
-print('Hello,World!')
+#Author:Student Nghia
+#My first Python lab in my CIS129 course
+#Prints a simple short story
+print('Hi,I'm interested in taking this class. I think it might a bit tought at the beginning since I take it online. Hopefully I can complete this course and obtain the degree!')
